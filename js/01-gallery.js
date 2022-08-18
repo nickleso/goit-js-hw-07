4e7f7860eddecfa891e6d046d9e357de98b03d91
Nick Leso <nickleso.work@gmail.com>
2022-08-18 14:29:37 +0300

    Create markup task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,64 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+// markup function
+function createGalleryMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+  <div class="gallery__item">
+    <a class="gallery__link" target="_self" title="Click to enlarge" rel="nofollow, noreferrer" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+    </a>
+  </div>
+  `;
+    })
+    .join('');
+}
+
+// click event function
+function onGalleryContainerClick(event) {
+  const isGalerryImage = event.target.classList.contains('.gallery__image');
+  const originalGalleryImageSource = event.target.dataset.source;
+  console.dir(event.target);
+
+  if (!isGalerryImage) {
+    return;
+  }
+
+  return console.dir(event.target.dataset.source);
+}
+
+// add active class on item function
+function addActiveClassOnItem(item) {
+  item.classList.add('is-active');
+}
+
+// remove active class from item function
+function removeActiveClassOnItem() {
+  const currentActiveItem = document.querySelector('.gallery__item.is-active');
+
+  if (currentActiveItem) {
+    currentActiveItem.classList.remove('.is-active');
+  }
+}
+
+//set bcg color function
+function setBodyBcgColor(color) {
+  document.body.style.backgroundColor = color;
+}
+
+setBodyBcgColor('beige');
